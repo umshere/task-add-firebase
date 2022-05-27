70ee16c991edb92d9d7737ccd6acc492b75c3bdf
Umesh <umshere@gmail.com>
2022-05-26 21:29:41 -0700

    firebase task add app

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,38 @@
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  function createTask(taskText,taskData) {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({
+      url: "https://react-course-94b9c-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }, createTask.bind(null, taskText));
+
+   
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
